f16a357c3814e7eb1ff74f7a844b1d754f3b7d55
Pedro Flores <pedro_23_96@hotmail.com>
2024-08-06 18:47:55 +0200

    feat: improve fonttools sample

--- fonttools/sample.py
+++ fonttools/sample.py
@@ -1,15 +1,55 @@
 from fontTools.ttLib import TTFont
+from fontTools.subset import Subsetter, Options
 
-# Abre la fuente
-font = TTFont('../fonts/Andén-Regular.otf')
+# Open the font
+with TTFont('../fonts/Andén-Regular.otf') as font:
 
-# List font tables
-print(font.keys())
+    """
+    List Font tables
+    """
+    print(font.keys())
+
+    """
+    List Font name
+    """
+    name = font.get('name')
+    for record in name.names:
+        if record.nameID == 1:
+            font_family_name = record.string.decode(record.getEncoding())
+            print(f"Font Family Name: {font_family_name}")
+            break
+    """
+    Change Font name
+    """
+    # Change font name
+    for record in font['name'].names:
+        if record.nameID == 1:
+            record.string = "New Font Name".encode(record.getEncoding())
+            break
+
+    # Save
+    # font.save('path/to/your/modified_fontfile.otf')
+
+    """
+    Subset
+    """
+    # subsetter configuration
+    options = Options()
+    subsetter = Subsetter(options=options)
+
+    # Define characters to maintain
+    subsetter.populate(text="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+    # Apply subset
+    subsetter.subset(font)
+
+    # Save
+    # font.save('path/to/your/subsetted_fontfile.otf')
+
+    """
+    Glyphs inspection
+    """
+    for glyph_name in font.glyphOrder:
+        print(glyph_name)
 
 
-# Retrieve font name
-name = font['name']
-for record in name.names:
-    if record.nameID == 1:
-        font_family_name = record.string.decode(record.getEncoding())
-        print(f"Font Family Name: {font_family_name}")
